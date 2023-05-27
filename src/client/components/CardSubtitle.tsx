2c56c3159b5cbeeb354d974412371de7d4c58c79
Chinh Do <chinh@doinby.co>
2023-05-27 22:49:36 +0700

    Fix error children key

--- src/client/components/CardSubtitle.tsx
+++ src/client/components/CardSubtitle.tsx
@@ -1,14 +1,15 @@
 import { IProject } from '../utils/interfaces';
 import { getStackColour } from '../utils/utilsFunctions';
 
-export default function CardSubtitle({ stacks, lastUpdated }: IProject) {
+export default function CardSubtitle({ title, stacks, lastUpdated }: IProject) {
 	return (
 		<div className='flex justify-between items-baseline px-12'>
 			<div className='flex items-baseline gap-4'>
 				{stacks instanceof Array &&
-					stacks.map((stack: string) => {
+					stacks.map((stack: string, idx: number) => {
 						return (
 							<p
+								key={`${title}-${idx}`}
 								className={`${getStackColour(
 									stack.toLowerCase()
 								)} font-header text-sm`}>
